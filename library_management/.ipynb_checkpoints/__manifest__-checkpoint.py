abc5b32da7d75c8299ec23f0e2cee64a3d3eb6f3
Veronica-Cucul <veronica@information-systems.ro>
2022-05-09 10:49:55 +0000

    [ADD] Basic Module

---
+++ library_management/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+         Management for the books and the clients of a library""",
+
+    'description': """
+        This module will allow us to have a good management for the clients and the books of our library
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Library',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+    'demo': [
+    ],
+}
