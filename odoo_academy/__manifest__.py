845cb20dab2e821b19c9ab98c6318b00e59b54ae
Veronica-Cucul <veronica@information-systems.ro>
2022-05-09 10:27:00 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Academy",
+
+    'summary': """
+         An academy to manage Training""",
+
+    'description': """
+       Academy Module to manage Training:
+       - Courses
+       - Session
+       - Attendees
+    """,
+
+    'author': "Odoo",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Training',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [],
+    'demo': [
+    ],
+}
