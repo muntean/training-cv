def741a3ae58601bf4ff3ca3f3533ddf4dd6092d
veronica <veronica@information-systems.ro>
2022-05-09 12:30:20 +0300

    New module library_management

---
+++ library_management/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+         Management for the books and the clients of a library""",
+
+    'description': """
+        This module will allow us to have a good management for the clients and the books of our library
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Library',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
