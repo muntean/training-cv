d1f003c8b02115e96797634198e671143d971503
veronica <veronica@information-systems.ro>
2022-05-10 10:03:00 +0300

    New fields on the book model

--- library_management/models/library_book.py
+++ library_management/models/library_book.py
@@ -7,4 +7,14 @@ class Book(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
 
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    edition_year = fields.Integer(string='Year of edition')
+    isbn = fields.Integer(string='ISBN', required=True)
+    genre = fields.Selection(string='Genre',
+                             selection=[('action_and_adventure', 'Action & Adventure'), ('fantasy', 'Fantasy'), ('romance', 'Romance'),
+                                        ('history', 'History'), ('travel', 'Travel')])
+    authors = fields.Char(string='Authors')
+    editors = fields.Char(string='Editors')
+    publisher = fields.Char(string='Publisher')
 
