7b6ece66168e0116348dfe55303a7db414f5fc72
Veronica-Cucul <veronica@information-systems.ro>
2022-05-16 07:37:55 +0000

    ORM and Fields

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -2,6 +2,7 @@
 
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -12,3 +13,24 @@ class Course(models.Model):
     description = fields.Text(string='Description')
     level = fields.Selection(string='Level', selection=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advanced', 'Advanced')], copy=False)
     active = fields.Boolean(string='Active', default=True)
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    additional_fee = fields.Float(string='Additional Fee', default=10.00)
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    session_ids = fields.One2many(comodel_name='academy.session', inverse_name='course_id', sting='Sessions')
+    
+    
+    @api.onchange('base_price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base Price cannot be set as Negative')
+            
+        self.total_price = self.base_price + self.additional_fee
+        
+        
+    @api.constrains('additional_fee')
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10.00:
+                raise ValidationError('Additional Fees cannot be less that 10.00: %s' % record.additional_fee)
