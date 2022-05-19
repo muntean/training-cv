899adebc0fc052e533e0fe0bcd3f566ebb20adb1
Veronica-Cucul <veronica@information-systems.ro>
2022-05-19 12:53:47 +0000

    Reports and Controllers

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -18,7 +18,7 @@
     'category': 'Training',
     'version': '0.1',
 
-    'depends': ['sale'],
+    'depends': ['sale', 'website'],
 
     'data': [
         'security/academy_security.xml',
@@ -28,7 +28,9 @@
         'views/session_views.xml',
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
-        'wizards/sale_wizard_views.xml'
+        'wizards/sale_wizard_views.xml',
+        'reports/session_report_templates.xml',
+        'views/academy_web_templates.xml'
     ],
     'demo': [
         'demo/academy_demo.xml',
